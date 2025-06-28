e34d1bbf71aac9b264c8926cf16b49a0a9f46714
Ethan Boes <deuceboes@gmail.com>
2025-06-28 13:58:50 -0400

    Create the playRound function

--- javascript.js
+++ javascript.js
@@ -15,3 +15,38 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let generalHumanChoice = humanChoice.toLowerCase();
+    if (generalHumanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("It's a tie.");
+        } else if (computerChoice === "paper") {
+            console.log("You lose! Paper beats rock");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats scissors");
+            humanScore++;
+        }
+    } else if (generalHumanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats rock");
+            humanScore++;
+        } else if (computerChoice === "paper") {
+            console.log("It's a tie.");
+        } else {
+            console.log("You lose! Scissors beat paper");
+            computerScore++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats paper");
+            humanScore++;
+        } else {
+            console.log("It's a tie.");
+        }
+    }
+}
